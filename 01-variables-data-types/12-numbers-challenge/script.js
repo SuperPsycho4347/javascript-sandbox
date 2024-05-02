e92c99e9d2148f7f4d64c23b423bca33ceeb7dce
Afaq Ahmad <superpsycho4347@gmail.com>
2024-05-02 10:57:53 +0500

    Completed challenge.

---
+++ 01-variables-data-types/12-numbers-challenge/script.js
@@ -0,0 +1,15 @@
+let x = Math.random() * 100 + 1;
+
+let y = Math.random() * 50 + 1;
+
+let mySum = x + y;
+
+let myDif = x - y;
+
+let myProd = x * y;
+
+let myDiv = x / y;
+
+let myRem = x % y;
+
+console.log(`${x} + ${y} = ${mySum}`);
