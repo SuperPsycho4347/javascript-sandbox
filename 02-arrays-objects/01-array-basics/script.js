ea5fb0f04b74577b2c5c0c13455e110f5640730a
Afaq Ahmad <superpsycho4347@gmail.com>
2024-05-02 14:15:32 +0500

    added array file.

---
+++ 02-arrays-objects/01-array-basics/script.js
@@ -0,0 +1,16 @@
+let x;
+
+// Array Literal
+const numbers = [12, 14, 17, 35];
+const mixed = [12, 'Hello', true, null]
+
+// Array Constructor
+const fruits = new Array('Apple', 'Orange','Guava');
+
+x = numbers[0];
+
+x = numbers[0] + numbers[3];
+
+fruits[2] = 'pair'
+
+console.log(fruits);
