579b23bc3a4b9f4cad3f914aee848c4a1e04dfc8
Afaq Ahmad <superpsycho4347@gmail.com>
2024-05-02 10:48:54 +0500

    added more files.

---
+++ 01-variables-data-types/10-numbers/script.js
@@ -0,0 +1,23 @@
+let x;
+
+const num = new Number(5.427);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = num.toFixed(2);
+
+x = num.toPrecision(3);
+
+x = num.toExponential(2);
+
+x = num.toLocaleString('ar-eg');
+
+x =num.valueOf(x);
+
+x = Number.MAX_VALUE;
+
+x = Number.MIN_VALUE;
+
+console.log(x);
