82a0b7847704bf93da2e07de9a32e6030f7ea499
Afaq Ahmad <superpsycho4347@gmail.com>
2024-05-02 11:15:49 +0500

    added date file.

---
+++ 01-variables-data-types/13-numbers-dates/script.js
@@ -0,0 +1,19 @@
+let d;
+
+d = new Date();
+
+d = d.toString();
+
+d = new Date(2021, 0, 10, 12, 30, 0);
+
+d = new Date('2021-07-19T12:30:00');
+
+d = new Date('07-10-2002');
+
+d = Date.now();
+
+d = new Date('07-10-2022 12:30:00');
+
+d = d.getTime();
+
+console.log(d);
