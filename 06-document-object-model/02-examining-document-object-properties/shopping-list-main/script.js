088134279de264c7aae9af0d1ee2905d68faf00e
Afaq Ahmad <superpsycho4347@gmail.com>
2024-07-02 16:20:18 +0500

    Commit

--- 06-document-object-model/02-examining-document-object-properties/shopping-list-main/script.js
+++ 06-document-object-model/02-examining-document-object-properties/shopping-list-main/script.js
@@ -5,23 +5,13 @@ output = document.all.length
 output = document.documentElement
 
 output = document.head
-output = document.body
-
-output = document.head.children
+output = document.body.children
 
 output = document.doctype
-output = document.URL
-output = document.characterSet
-output = document.contentType
+output = document.domain
 
-output = document.forms[0].id
-output = document.forms[0].method
-output = document.forms[0].action
-
-// document.forms[0].id = 'new-id'
+output = document.forms
 
 output = document.links
-
-output = document.images[0]
 
 console.log(output);
--- 06-document-object-model/03-dom-selectors-single-element/shopping-list-main/script.js
+++ 06-document-object-model/03-dom-selectors-single-element/shopping-list-main/script.js
@@ -1,10 +1,27 @@
-document.getElementById('app-title').title = 'Shopping List'
-document.getElementById('app-title').setAttribute('class', 'title');
+// document.getELementById()
+
+console.log(document.getElementById('app-title'));
+console.log(document.getElementById('app-title').getAttribute('id'));
+
+// set attributes
+document.getElementById('app-title').title = "Shopping List"
+document.getElementById('app-title').setAttribute('class', 'title')
 
 const title = document.getElementById('app-title')
-
-console.log(title);
 
 // get/change content
 console.log(title.textContent);
 title.textContent = 'Hello World'
+title.innerText = 'Hello Again'
+title.innerHTML = '<strong>Shopping List</strong>'
+
+// change styles
+title.style.color = 'red'
+title.style.backgroundColor = 'black'
+title.style.padding = '10px'
+title.style.borderRadius = '10px'
+
+// document.query.selector()
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('#app-title'));
+console.log(document.querySelector('.container'));
